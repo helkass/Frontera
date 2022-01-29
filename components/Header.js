0749d783def33cc5d31d45ac498e1534d3751577
helkass <helkaseptyawan@gmail.com>
2022-01-29 23:40:06 +0700

    modified header

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
     <nav className="px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link href="/">
-          <a crossOrigin="anonymous" className="flex">
+          <a className="flex">
             <span className="self-center text-xl uppercase font-semibold whitespace-nowrap dark:text-white">
               Frontera
             </span>
--- pages/_document.js
+++ pages/_document.js
@@ -10,20 +10,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito&display=swap"
             rel="stylesheet"
-            crossOrigin="anonymous"
           />
         </Head>
         <body>
